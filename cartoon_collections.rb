5ffc951ae00b3ce25d1d5bbbdf83916804ec0e22
edmundo papi <edmundopierre@yahoo.com>
2020-03-28 18:14:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -40,6 +40,7 @@ def find_the_cheese(array)# code an argument here
   array.any? do |value|
     if cheese_types.include?(value) == true
           return value
+    end
    end
   else
       return nil
