c05f8f9a220ed8d277e55f694ea96e26c21a91d1
edmundo papi <edmundopierre@yahoo.com>
2020-03-28 17:30:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,14 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if (array& cheese_types).empty? == true
+      return (array& cheese_types)
+
+  else
+      return nil
+  end    
 end
